0dc66290f8969822b0d22ef8ce7d95509040a4a9
Natalie <38893790+ceruleanat@users.noreply.github.com>
2023-03-31 11:46:11 -0700

    Add files via upload

---
+++ initials.py
@@ -0,0 +1,8 @@
+# Your first and last name.
+print("NN    N  FFFFF")
+print("N N   N  F")
+print("N  N  N  FFF")
+print("N   N N  F")
+print("N    NN  F")
+
+# A fun fact: I love karaoke, burritos, my doggos + snake, and video games.
